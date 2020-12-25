07336fc71a83b5ec525fba3cdecca92cc09239f0
shuklav706 <shuklav706@gmail.com>
2020-12-25 15:21:44 +0530

    second commit

---
+++ Add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Add
+{
+public static void main(String args[])
+{
+Scanner kb=new Scanner(System.in);
+int a,b,c;
+System.out.println("Enter first no");
+a=kb.nextInt();
+System.out.println("Enter second no");
+b=kb.nextInt();
+c=a+b;
+System.out.println("Enter first no"+a);
+System.out.println("Enter second no"+b);
+System.out.println("sum is"+c);
+}
+}
